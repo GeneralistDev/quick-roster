0f08a403f818126ffcf4f2dcd1f036778bfd75b7
Daniel Parker <dparker.tech@gmail.com>
2017-04-02 11:33:33 +1000

    bad bash fix

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 export SERVERLESS_STAGE=$1
+export IAM_USER=$2
 
 if [ -z ${SERVERLESS_STAGE} ]; then
     echo "Please set the serverless stage eg. dan"
+    exit 1
+fi
+
+if [ -z ${IAM_USER} ]; then
+    echo "Please set the IAM User eg. daniel"
     exit 1
 fi
 
