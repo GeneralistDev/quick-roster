cc9c5e7febc66a0d9c878642baac08e57c793e14
Dale Salter <dalejsalter@outlook.com>
2017-04-02 14:08:36 +1000

    ADDED: Removed eslint ignore

--- frontend/src/constants/Config.js
+++ frontend/src/constants/Config.js
@@ -4,7 +4,7 @@ let year = date.getFullYear();
 const APPCONFIG = {
   brand: 'Material',
   user: 'Lisa',
-  year: year,
+  year,
   productLink: 'https://themeforest.net/item/material-design-reactjs-admin-web-app-with-bootstrap-4/19582625',
   AutoCloseMobileNav: true,                           // true, false. Automatically close sidenav on route change (Mobile only)
   color: {
@@ -26,6 +26,6 @@ const APPCONFIG = {
     colorOption: '14',                              // String: 11,12,13,14,15,16; 21,22,23,24,25,26; 31,32,33,34,35,36
     theme: 'light',                                 // light, gray, dark
   }
-}
+};
 
 module.exports = APPCONFIG;
