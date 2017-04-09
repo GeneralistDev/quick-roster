6dcd0fa8fcb783f93dc2e9817d638055733f1a47
richardroy <richardroygibson@gmail.com>
2017-04-09 12:57:06 +1000

    cleaned routes

--- frontend/src/routes/app/index.js
+++ frontend/src/routes/app/index.js
@@ -3,13 +3,8 @@ module.exports = {
     getChildRoutes(partialNextState, cb) {
         require.ensure([], (require) => {
             cb(null, [
-                require('./routes/charts'),
                 require('./routes/dashboard'),
-                require('./routes/ecommerce'),
-                require('./routes/forms'),
                 require('./routes/pageLayouts'),
-                require('./routes/pages'),
-                require('./routes/tables'),
                 require('./routes/ui'),
             ])
         })
